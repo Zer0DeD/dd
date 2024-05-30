3089f90883db5a6a81188effd0a3ad6d786c4bdd
Vladislav Syrochkin <vsyroc@gmail.com>
2024-05-30 22:38:02 +0500

    fix http

--- src/api/intercetors.ts
+++ src/api/intercetors.ts
@@ -1,7 +1,7 @@
 import axios, { type CreateAxiosDefaults } from 'axios';
 
 const options: CreateAxiosDefaults = {
-  baseURL: 'https://188.226.94.79:8000',
+  baseURL: 'https://188.226.94.79',
   headers: {
     'Content-Type': 'application/json',
   },
