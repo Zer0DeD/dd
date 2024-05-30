693e7a7c1efd29b284d1ffd80b4b9749f6c56a25
Vladislav Syrochkin <vsyroc@gmail.com>
2024-05-30 22:58:04 +0500

    NODE_TLS_REJECT_UNAUTHORIZED = 0

--- src/api/intercetors.ts
+++ src/api/intercetors.ts
@@ -1,14 +1,15 @@
 import axios, { type CreateAxiosDefaults } from 'axios';
 const https = require('https');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const options: CreateAxiosDefaults = {
   baseURL: 'https://188.226.94.79',
   headers: {
     'Content-Type': 'application/json',
   },
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false,
-  }),
+  // httpsAgent: new https.Agent({
+  //   rejectUnauthorized: false,
+  // }),
 };
 
 const axiosClassic = axios.create(options);
