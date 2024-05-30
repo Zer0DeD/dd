48ee2099a31f3face747885694057b578e9807f5
Vladislav Syrochkin <vsyroc@gmail.com>
2024-05-30 22:55:59 +0500

    add reject

--- src/api/intercetors.ts
+++ src/api/intercetors.ts
@@ -1,10 +1,14 @@
 import axios, { type CreateAxiosDefaults } from 'axios';
+const https = require('https');
 
 const options: CreateAxiosDefaults = {
   baseURL: 'https://188.226.94.79',
   headers: {
     'Content-Type': 'application/json',
   },
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
 };
 
 const axiosClassic = axios.create(options);
