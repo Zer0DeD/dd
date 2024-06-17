c7bda33fad37dd59ac6dd9d088755f5389a1f60e
Vladislav Syrochkin <vsyroc@gmail.com>
2024-06-17 16:33:17 +0000

    refactor(header)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,34 +3,6 @@ const { Header } = Layout;
 
 export default function DDHeader() {
   return (
-    // <Header
-    //   style={{
-    //     position: 'sticky',
-    //     top: 0,
-    //     zIndex: 1,
-    //     width: '100%',
-    //     display: 'flex',
-    //     alignItems: 'center',
-    //     justifyContent: 'space-between',
-    //   }}
-    // >
-    //   {/* <Typography.Title
-    //     level={2}
-    //     style={{ color: 'whitesmoke', padding: '20px' }}
-    //   >
-    //     Цифровая кафедра
-    //   </Typography.Title> */}
-    //   <h2
-    //     style={{
-    //       color: 'whitesmoke',
-    //       fontFamily: 'Helvetica Neue',
-    //       fontSize: '24px',
-    //       fontWeight: 'bold',
-    //     }}
-    //   >
-    //     Цифровая кафедра
-    //   </h2>
-    // </Header>
     <header
       style={{
         backgroundColor: '#000',
